d4ad3d9de282d97ecdef069e1c38d63c1fb678cd
Ryo Manzoku <manzoku.ryo@gmail.com>
2017-03-06 09:41:27 +0000

    It works
    why lambda_function to ambda_function

--- lambda_function.py
+++ lambda_function.py
@@ -1,14 +1,11 @@
 import json
+import os
 
 print('Loading function')
 
 
 def lambda_handler(event, context):
-    #print("Received event: " + json.dumps(event, indent=2))
-    print("value1 = " + event['key1'])
-    print("value2 = " + event['key2'])
-    print("value3 = " + event['key3'])
-    return event['key1']  # Echo back the first key value
-#raise Exception('Something went wrong')
 
-
+    rds_instance_name = os.environ['rds_instance_name']
+    print("rds instance = " + rds_instance_name)
+    return "Fin"
